14010f3fcdd957a864e404704d7e8185ed512515
Nikesh Gangwar <146576953+NikeshGangwar41@users.noreply.github.com>
2026-05-13 17:06:08 +0530

    Add initial implementation of Service class and create app and server files

---
+++ main.js
@@ -0,0 +1,7 @@
+class Service {
+  constructor(type, firstHourCost, additionalHourCost) {
+    this.type = type;
+    this.firstHourCost = firstHourCost;
+    this.additionalHourCost = additionalHourCost;
+  }
+}
